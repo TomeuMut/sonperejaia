d3ee6bf0c973aaeb8b3cf26ec432bfdab9fb3a11
Bartomeu Mut Vidal <tomeu95@hotmail.com>
2023-05-19 18:54:01 +0200

    Add October in the Project

---
+++ themes/son-pere-jaia/webpack.mix.js
@@ -0,0 +1,21 @@
+const mix = require('laravel-mix')
+const tailwindcss = require('tailwindcss'); /* Add this line at the top */
+
+mix.setPublicPath('./assets/')
+mix.setPublicPath('./assets/')
+
+mix.disableSuccessNotifications();
+
+
+mix.js('assets/js/bundle/index.js', 'js/main.js')
+    .sass('assets/scss/app.scss', 'css').options({
+    processCssUrls: false,
+    postCss: [tailwindcss('./tailwind.config.js')],
+})
+
+
+mix.browserSync({
+    proxy: 'sonperejaia.test/',
+    open: false,
+    files: ['./**/*.htm', "./**/*.js", './**/*.scss', './../../plugins/**/*.htm']
+})
